e23711bc85859cc3c4c95372c1d16f8c2a54baae
Raul Almeida <raulnara77@gmail.com>
2018-03-19 18:28:14 -0300

    Iniciando

---
+++ TADSequencia/src/TSequencia.java
@@ -0,0 +1,130 @@
+
+public class TSequencia implements Sequencia {
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Object elemAtRank(int r) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object replaceAtRank(int r, Object o) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object insertAtRank(int r, Object o) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object removeAtRank(int r, Object o) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public No isFirst(No n) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public No isLast(No n) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public No first() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public No last() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public No before(No a) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public No after(No p) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void replaceElem(No n, Object o) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void swapElem(No n, Object q) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Object insertBefore(No n, Object o) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object insertAfter(No n, Object o) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void insertFirst(No o) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void insertLast(No o) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Object remove(No n) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public No atRank(int r) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object rankOf(No n) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
