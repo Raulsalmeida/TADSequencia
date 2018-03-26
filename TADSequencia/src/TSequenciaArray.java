8d2ee4ab03469d87d6f3562f85f548392a11636d
Davi Souto <davisouto.if@gmail.com>
2018-03-26 18:26:52 -0300

    Alterações da Sequencia Array

---
+++ TADSequencia/src/TSequenciaArray.java
@@ -0,0 +1,135 @@
+
+public class TSequenciaArray implements Sequencia {
+
+	private No v[];
+	private No fim;
+	private int tamanho;
+	
+	
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Object elemAtRank(int r) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object replaceAtRank(int r, Object o) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void insertAtRank(int r, Object o) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Object removeAtRank(int r, Object o) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isFirst(No n) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isLast(No n) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public No first() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public No last() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public No before(No n) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public No after(No n) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void replaceElem(No n, Object o) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void swapElem(No n, No q) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Object insertBefore(No n, Object o) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object insertAfter(No n, Object o) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void insertFirst(No o) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void insertLast(No o) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Object remove(No n) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public No atRank(int r) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object rankOf(No n) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- TADSequencia/src/Vetor.java
+++
@@ -1,9 +0,0 @@
-
-public interface Vetor {
-	public int size();
-	public boolean isEmpty();
-	public Object elemAtRank(int r);
-	public Object replaceAtRank(int r, Object o);
-	public void insertAtRank(int r, Object o);
-	public Object removeAtRank(int r, Object o);
-}
